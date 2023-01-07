c0062f5c57a3c8a998b69aee4faa21f6903e7a37
Matthias <mroeckl@googlemail.com>
2023-01-07 15:50:44 +0100

    Testing mdx

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -3,7 +3,7 @@ import type { GatsbyConfig } from "gatsby";
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `Write-ups on technical topics`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://techblog.roeckls.de`
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
---
+++ src/pages/post_gatsby-on-github-pages.md
@@ -0,0 +1,7 @@
+---
+slug: "/gatsby-on-github-pages"
+date: "2023-01-07"
+title: "My blog with Gatsby on Github Pages"
+---
+
+# Hello, world!
